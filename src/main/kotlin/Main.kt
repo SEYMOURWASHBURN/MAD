a23865421f9b0d46507ef200f9618a77380613df
Anna Taylor <overtime7@aol.com>
2023-03-30 20:34:39 -0400

    Ethan's Birthday Card

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,17 @@
+fun main(args: Array<String>) {
+    println("  _________ ")
+    println(" |         |")
+    println(" |  Happy  |")
+    println(" |   22nd  |")
+    println(" | Birthday|")
+    println(" |  ETHAN! |")
+    println(" |_________|")
+    println("     ||     ")
+    println("     ||     ")
+    println("     ||     ")
+    println("   \\====/   ")
+
+    // Try adding program arguments via Run/Debug configuration.
+    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
+    println("Program arguments: ${args.joinToString()}")
+}
